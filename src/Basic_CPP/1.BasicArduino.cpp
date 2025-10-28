d11b623428404095096fad146b51d01467a7be11
Kendo-C12 <sukrit.limp@gmail.com>
2025-10-29 00:00:23 +0700

    Update 1.BasicArduino.cpp

--- src/Basic_CPP/1.BasicArduino.cpp
+++ src/Basic_CPP/1.BasicArduino.cpp
@@ -6,14 +6,16 @@ void setup(){
     // Initialize Serial port with baudrate 115200
     Serial.begin(115200);
 
-    // Print "Hello World" which can be viewed via serial monitor
+    /* Print "Hello World" which can be 
+		viewed via serial monitor */
     Serial.print("Hello World");
 }
 
 // Repeat continuously forever
 void loop(){
 
-    // Print "This is blank line" and move to a new line automatically
+    /* Print "This is blank line" and move 
+		to a new line automatically */
     Serial.println("This is blank line");
 
     // Pause the entire program for 100 milliseconds
